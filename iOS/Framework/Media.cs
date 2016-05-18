6263d48743fbabf98a79e887c7972cf879fa92a2
Thetyne <thetyne@live.fr>
2016-05-18 16:40:32 +0200

    Possibilty to select user profile picture and upload it to server #19
    iOS scale image

--- iOS/Framework/Media.cs
+++ iOS/Framework/Media.cs
@@ -13,28 +13,42 @@ namespace BodyReport.iOS.Framework
     {
         public override void CompressImageAsPng(string imagePath, string compressedImagePath, int? maxSize)
         {
-            //TODO scale image
             if (File.Exists(compressedImagePath))
                 File.Delete(compressedImagePath);
 
-            CGImage cgImage = UIImage.FromFile(imagePath).CGImage;
-            nint originWidth = cgImage.Width;
-            nint originHeight = cgImage.Height;
-            var bitsPerComponent = cgImage.BitsPerComponent;
-            var bytesPerRow = cgImage.BitsPerPixel;
-            var colorSpace = cgImage.ColorSpace;
-            var bitmapInfo = cgImage.BitmapInfo;
-            using (var context = new CGBitmapContext(null, originWidth, originHeight, bitsPerComponent, bytesPerRow, colorSpace, bitmapInfo))
-            {
-                context.InterpolationQuality = CGInterpolationQuality.High;
-                context.DrawImage(new CGRect(CGPoint.Empty, new CGSize(originWidth, originHeight)), cgImage);
-                using(var imageRef = context.ToImage())
-                {
-                    UIImage convertedImage = new UIImage(imageRef);
-                    NSData convertedImageData = convertedImage.AsPNG();
-                    convertedImageData.Save(compressedImagePath, true);
-                }
-            }
+			NSData convertedImageData;
+            UIImage image = UIImage.FromFile(imagePath);
+			if (!maxSize.HasValue)
+			{
+				convertedImageData = image.AsPNG ();
+				convertedImageData.Save (compressedImagePath, true);
+				return;
+			}
+
+			CGSize newSize = new CGSize ((nfloat)maxSize, (nfloat)maxSize);
+			if (image.Size.Width < newSize.Width && image.Size.Height < newSize.Height)
+			{
+				convertedImageData = image.AsPNG ();
+				convertedImageData.Save (compressedImagePath, true);
+				return;
+			}
+
+			//Determine the scale factors
+			nfloat widthScale = newSize.Width/image.Size.Width;
+			nfloat heightScale = newSize.Height/image.Size.Height;
+
+			nfloat scaleFactor;
+
+			//The larger scale factor will scale less (0 < scaleFactor < 1) leaving the other dimension hanging outside the newSize rect
+			if (widthScale > heightScale)
+				scaleFactor = widthScale;
+			else 
+				scaleFactor = heightScale;
+			CGSize scaledSize = new CGSize(image.Size.Width * scaleFactor, image.Size.Height * scaleFactor);
+
+			UIImage convertedImage = UIImage.FromImage (image.CGImage).Scale (scaledSize);
+			convertedImageData = convertedImage.AsPNG();
+			convertedImageData.Save(compressedImagePath, true);
         }
     }
 }
