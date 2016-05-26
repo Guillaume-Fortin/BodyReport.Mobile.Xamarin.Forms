752dbb1e76e9c576e387092a3254f7033a9f0f58
Thetyne <thetyne@live.fr>
2016-05-26 15:33:59 +0200

    Force orientation to portait in Android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -15,7 +15,7 @@ using BodyReport.Droid.Framework;
 
 namespace BodyReport.Droid
 {
-	[Activity (MainLauncher = false, Label = "BodyReport", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (MainLauncher = false, Label = "BodyReport", Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
