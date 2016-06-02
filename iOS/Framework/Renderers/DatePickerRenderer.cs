5d5220d66a5ef2dbb12b34864463460efe078db9
Thetyne <thetyne@live.fr>
2016-06-02 19:27:33 +0200

    iOS support #16
    Remove border on DatePicker and TimePicker

---
+++ iOS/Framework/Renderers/DatePickerRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(BodyReport.iOS.Framework.Renderers.DatePickerRenderer))]
+namespace BodyReport.iOS.Framework.Renderers
+{
+	public class DatePickerRenderer : Xamarin.Forms.Platform.iOS.DatePickerRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+		{
+			base.OnElementChanged (e);
+
+			if (this.Control != null)
+			{
+				this.Control.BorderStyle = UIKit.UITextBorderStyle.None;
+			}
+		}
+	}
+}
+
---
+++ iOS/Framework/Renderers/TimePickerRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using BodyReportMobile.Presenter.Framework.Controls;
+using BodyReport.iOS;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TimePicker), typeof(BodyReport.iOS.Framework.Renderers.TimePickerRenderer))]
+namespace BodyReport.iOS.Framework.Renderers
+{
+	public class TimePickerRenderer : Xamarin.Forms.Platform.iOS.TimePickerRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<TimePicker> e)
+		{
+			base.OnElementChanged (e);
+
+			if (this.Control != null)
+			{
+				this.Control.BorderStyle = UIKit.UITextBorderStyle.None;
+			}
+		}
+	}
+}	
+
