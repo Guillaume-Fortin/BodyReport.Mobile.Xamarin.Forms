2555d9c1eab2834ac363dded9570c09d8f61805a
Thetyne <thetyne@live.fr>
2016-06-23 00:42:59 +0200

    Prepare Publish to Android Store #38

--- Droid/MainApplication.cs
+++ Droid/MainApplication.cs
@@ -8,7 +8,11 @@ using Plugin.CurrentActivity;
 namespace BodyReport.Droid
 {
 	//You can specify additional application information in this attribute
-    [Application]
+    #if DEBUG
+    [Application(Debuggable=true)]
+    #else
+    [Application(Debuggable = false)]
+    #endif
     public class MainApplication : Application, Application.IActivityLifecycleCallbacks
     {
         public MainApplication(IntPtr handle, JniHandleOwnership transer)
